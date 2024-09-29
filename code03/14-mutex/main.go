fcbe1616781ef741aba8ae94be30ef7d0fb91ab1
内山健一 <huavcjj0217@icloud.com>
2024-09-29 22:05:43 +0900

    chore: Add Go learning notes

--- code03/14-mutex/main.go
+++ code03/14-mutex/main.go
@@ -7,15 +7,23 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
+	var mu sync.Mutex
 	var i int
 	wg.Add(2)
+
 	go func() {
 		defer wg.Done()
-		i++
+		mu.Lock()
+		defer mu.Unlock()
+		// i++
+		i = 1
 	}()
 	go func() {
 		defer wg.Done()
-		i++
+		mu.Lock()
+		defer mu.Unlock()
+		// i++
+		i = 2
 	}()
 	wg.Wait()
 	fmt.Println(i)
