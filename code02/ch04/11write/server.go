b7f10d579e7d87f49a95e48c84f103c166da2e25
huavcjj <huavcjj0217@icloud.com>
2024-11-20 20:44:40 +0900

    feat

--- code02/ch04/11write/server.go
+++ code02/ch04/11write/server.go
@@ -13,19 +13,19 @@ type Post struct {
 
 func writeExample(w http.ResponseWriter, r *http.Request) {
 	str := `<html>
-<head><title>Go Web Programming</title></head>
-<body><h1>Hello World</h1></body>
-</html>`
+	<head><title>Go Web Programming</title></head>
+	<body><h1>Hello, Gophers!</h1></body>
+	</html>`
 	w.Write([]byte(str))
 }
 
 func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(501)
-	fmt.Fprintln(w, "そのようなサービスはありません。ほかを当たってください")
+	fmt.Println(w, "No such service, try next door")
 }
 
 func headerExample(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Location", "http://google.com")
+	w.Header().Set("Location", "http://www.google.com")
 	w.WriteHeader(302)
 }
 
@@ -33,7 +33,7 @@ func jsonExample(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	post := &Post{
 		User:    "Sau Sheong",
-		Threads: []string{"1番目", "2番目", "3番目"},
+		Threads: []string{"first", "second", "third"},
 	}
 	json, _ := json.Marshal(post)
 	w.Write(json)
@@ -48,4 +48,9 @@ func main() {
 	http.HandleFunc("/redirect", headerExample)
 	http.HandleFunc("/json", jsonExample)
 	server.ListenAndServe()
+	// http.ListenAndServe(":8080", nil)
 }
+
+// % curl -i 127.0.0.1:8080/write
+// % curl -i 127.0.0.1:8080/writeheader
+// % curl -i 127.0.0.1:8080/redirect
